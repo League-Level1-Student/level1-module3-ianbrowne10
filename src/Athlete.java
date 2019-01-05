e83a6c66160305143f49cb132bf5044d4fa34957
League <League@AdminisorsiMac2.attlocal.net>
2019-01-05 11:24:58 -0800

    Athlete(IP)

---
+++ src/Athlete.java
@@ -0,0 +1,32 @@
+public class Athlete {
+
+	static int nextBibNumber;
+	static String raceLocation = "New York";
+	static String raceStartTime = "9.00am";
+
+	String name;
+	int speed;
+	static int bibNumber;
+
+	Athlete(String name, int speed) {
+		this.name = name;
+		this.speed = speed;
+		for (int i = 0; i < 2; i++) {
+			System.out.println(bibNumber +=1);
+		}
+	}
+
+	public static void main(String[] args) {
+		// create two athletes //print their names, bibNumbers, and the location of
+		// their race. }
+	Athlete("Bill", 7);
+		
+	
+	Athlete("Bob", 9);
+	}
+
+	private static void Athlete(String string, int i) {
+		// TODO Auto-generated method stub
+		
+	}
+}
