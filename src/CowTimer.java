cbdb89e6f0219e3aa633474455b0f1328fa5a741
An idiot <ur@mom.com>
2019-03-03 12:55:13 -0800

    GridWorld, CowTimer(IP)

--- src/CowTimer.java
+++ src/CowTimer.java
@@ -15,9 +15,13 @@ public class CowTimer {
 
 	public static void main(String[] args) throws InterruptedException {
 		/* 1. Make a CowTimer, set the time and start it. */
+CowTimer cow = new CowTimer();
+	cow.setTime(1);
+	cow.start();
+		
+	
 
 	}
-
 	private int minutes;
 
 	public void setTime(int minutes) {
@@ -30,7 +34,10 @@ public class CowTimer {
 		 * 2. Count down the minutes, print the current minute then sleep for 60 seconds
 		 * using Thread.sleep(int milliseconds).
 		 */
-
+for(int i = 0; i < minutes; i++) {
+Thread.sleep(1000);	
+System.out.println(minutes-i);
+}
 		/*
 		 * 3. When the timer is finished, use the playSound method to play a moo sound.
 		 * You can use the .wav file in the default package, or you can download one
--- src/extra/Gridworld.java
+++ src/extra/Gridworld.java
@@ -1,18 +1,45 @@
 package extra;
 
+import java.awt.Color;
+import java.util.Random;
+
 import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
 public class Gridworld {
 
+	public static void main(String[] args) {
+		int random = new Random().nextInt(10);
+		World w = new World();
+		Bug b = new Bug();
+		Bug b2 = new Bug();
+		Flower f = new Flower();
+		Flower f2 = new Flower();
+		Color color = new Color(50, 25, 150);
+		b2.setColor(color);
+		b2.act();
+		b2.act();
+		Location l = new Location(2, 7);
+		Location q = new Location(random, random);
+		Location l2 = new Location(2, 8);
+		Location l3 = new Location(2, 6);
+		for (int y = 0; y < 10; y++) {
 
-public static void main(String[] args) {
-	World w = new World();
-Bug b = new Bug();
-Location l = new Location(2,7);	
-w.show();
+			for (int x = 0; x < 10; x++) {
+				Location i = new Location(x, y);
+				Flower d = new Flower();
+				w.add(i, d);
+			}
+		}
 
-	w.add(l,b);
-}
+		w.show();
+
+		w.add(q, b2);
+		w.add(l, b);
+		w.add(l2, f);
+		w.add(l3, f2);
+
+	}
 }
